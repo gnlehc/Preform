a1f6566293fd1dbe5fe65bae5252bd26ce142e83
akkasel <akkaselr@gmail.com>
2024-01-30 16:08:34 +0700

    some changes

--- lib/widgets/prev_interview_card.dart
+++ lib/widgets/prev_interview_card.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class CardExample extends StatefulWidget {
+void main() {
+  runApp(MaterialApp(
+      home: PrevInterviewCard(
+    progressValue: 0.1,
+    numberPercentage: 10,
+  )));
+}
+
+class PrevInterviewCard extends StatefulWidget {
   final double progressValue;
   final int numberPercentage;
 
-  const CardExample({
+  const PrevInterviewCard({
     Key? key,
     required this.progressValue,
     required this.numberPercentage,
   }) : super(key: key);
 
   @override
-  _CardExampleState createState() => _CardExampleState();
+  _PrevInterviewCardState createState() => _PrevInterviewCardState();
 }
 
-class _CardExampleState extends State<CardExample> {
-
-
+class _PrevInterviewCardState extends State<PrevInterviewCard> {
   @override
   Widget build(BuildContext context) {
-    return Stack(alignment: const Alignment(0.6, 0.6),
-        children: [
+    return Stack(alignment: const Alignment(0.6, 0.6), children: [
       // pake stack supaya iconnya ada di atas cardnya
       // Add icon nya disini
-
 
       Card(
           margin: const EdgeInsets.all(25),
@@ -33,18 +38,51 @@ class _CardExampleState extends State<CardExample> {
             borderRadius: const BorderRadius.all(Radius.circular(12)),
           ),
           child: Container(
+            padding: EdgeInsets.all(16.0),
+            width: 300,
             // borderRadius: BorderRadius.circular(10.0) ,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                const ListTile(
-                  // leading: ImageSection(image: 'images/code_icon_revisipng.png'),
-                  title: Text('Frontend Developer'),
-                  subtitle: Text('Entry Level'),
+                Row(
+                  children: <Widget>[
+                    SvgPicture.asset("lib/images/data_chart_icon.svg"),
+                    SizedBox(width: 13),
+                  ],
                 ),
                 Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: ClipRRect(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Data Scientist',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 4),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Entry-Level',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 4),
+                Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black, // Set the border color
+                        width: 2.0, // Set the border width
+                      ),
+                      borderRadius: BorderRadius.circular(10), // Set the border radius
+                    ),
+                    child: ClipRRect(
                     // Wrap LinearProgressIndicator with ClipRRect
                     borderRadius: BorderRadius.circular(10.0),
                     // Adjust the radius as needed
@@ -56,9 +94,11 @@ class _CardExampleState extends State<CardExample> {
                           AlwaysStoppedAnimation<Color>(Color(0xFFFF6C37)),
                     ),
                   ),
+                  )
+
                 ),
                 Padding(
-                  padding: EdgeInsets.only(left: 16.0, bottom: 16.0),
+                  padding: EdgeInsets.only(bottom: 16.0),
                   // Adjust the left margin as needed
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
@@ -97,9 +137,7 @@ class _CardExampleState extends State<CardExample> {
               ],
             ),
           )),
-          // Image.asset('images/iconCode.svg'),
-
+      // Image.asset('images/iconCode.svg'),
     ]);
   }
 }
-

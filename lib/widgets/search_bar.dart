0862136190aed368e161d9db0385a9e618ab8dd6
jesslynamanda13 <jesslynmulyawan@gmail.com>
2024-01-30 19:25:47 +0700

    [On Progress]: Searchbar

---
+++ lib/widgets/search_bar.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatefulWidget{
+  final List<String> industries = ['All jobs', 'Data & Product Management', 'Business dev & sales', '', 'Education', 'Retail', 'Telecommunications', 'Real Estate'];
+
+
+}

236535666e6e5fbd7bcb9f90ec7ddd926caa401f
Chelsea Ng <Chelseang40@Gmail.com>
2024-01-31 13:19:00 +0700

    Fix: dashboard and feedback Page

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:preform/pages/login.dart';
 import 'package:preform/pages/signup.dart';
 import 'package:preform/pages/splashscreen.dart';
 import 'package:preform/user_auth/user_provider.dart';
-import 'package:preform/widgets/navbar.dart';
 import 'package:provider/provider.dart';
 
 Future main() async {
@@ -24,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: '/',
-      home: const MyHomePage(),
+      home: const Home(),
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         textTheme: GoogleFonts.dmSansTextTheme(
@@ -49,22 +48,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
+  // int _currentIndex = 0;
 
-  void _onNavbarItemTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
+  // void _onNavbarItemTapped(int index) {
+  //   setState(() {
+  //     _currentIndex = index;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SplashScreen(),
-      bottomNavigationBar: Navbar(
-        currentIndex: _currentIndex,
-        onTap: _onNavbarItemTapped,
-      ),
+      // bottomNavigationBar: Navbar(
+      //   currentIndex: _currentIndex,
+      //   onTap: _onNavbarItemTapped,
+      // ),
     );
   }
 }
